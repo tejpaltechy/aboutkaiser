8bf4c68974125d05932c1452ea8c74d98afcac0d
David Nuescheler <david@nuescheler.ch>
2022-05-06 12:23:11 -0600

    feat: add columns

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -607,6 +607,17 @@ function buildTOC(main) {
   }
 }
 
+function buildHeroBlock(main) {
+  const h1 = main.querySelector('h1');
+  const picture = main.querySelector('picture');
+  // eslint-disable-next-line no-bitwise
+  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
+    const section = document.createElement('div');
+    section.append(buildBlock('hero', { elems: [picture, h1] }));
+    main.prepend(section);
+  }
+}
+
 function loadHeader(header) {
   const headerBlock = buildBlock('header', [
     ['<div>Nav</div>', '<div>/northern-california/nav</div>'],
@@ -634,6 +645,7 @@ function buildAutoBlocks(main) {
     if (getMetadata('template') === 'structured-content') {
       buildTOC(main);
     }
+    buildHeroBlock(main);
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Auto Blocking failed', error);
