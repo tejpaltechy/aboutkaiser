1981fcca1ebf69456dc1154f61423793552bf56d
Katie Sexton <ksexton@adobe.com>
2022-05-06 19:33:12 -0600

    refactor: nav overhaul

--- northern-california/health-wellness/cancer-care/blocks/header/header.js
+++ northern-california/health-wellness/cancer-care/blocks/header/header.js
@@ -1,30 +1,212 @@
-import { readBlockConfig, decorateIcons } from '../../scripts/scripts.js';
+import { readBlockConfig, decorateIcons, toClassName } from '../../scripts/scripts.js';
 
-function onMediaChange() {
-  const mq = window.matchMedia('(min-width: 900px)');
-  const header = document.querySelector('header');
-  const nav = header.querySelector('.nav');
-  if (mq.matches) {
-    header.classList.remove('collapsed');
-    if (nav) nav.setAttribute('aria-expanded', true);
-  } else if (!mq.matches) {
-    header.classList.add('collapsed');
-    if (nav) nav.setAttribute('aria-expanded', false);
+function closeMenu(el) {
+  el.setAttribute('aria-expanded', false);
+}
+
+function openMenu(el) {
+  el.setAttribute('aria-expanded', true);
+}
+
+function toggleMenu(e) {
+  const btn = e.target.closest('[role="button"]');
+  const expanded = btn.getAttribute('aria-expanded') === 'true';
+  if (expanded) {
+    closeMenu(btn);
+  } else {
+    openMenu(btn);
   }
 }
 
-async function openLanguageDialog() {
-  const dialog = document.createElement('aside');
-  dialog.classList.add('language-picker-dialog');
+function onMediaChange() {
+  const mq = window.matchMedia('(min-width: 900px)');
+  if (mq.matches) {
+    document.querySelector('header .nav').setAttribute('aria-expanded', true);
+    document.body.style.overflowY = '';
+  } else {
+    document.querySelector('header .nav').setAttribute('aria-expanded', false);
+  }
+}
 
-  const resp = await fetch('/northern-california/language-picker.plain.html');
+/**
+ * collapses all open nav sections
+ * @param {Element} sections The container element
+ */
+function collapseAllNavSections(sections) {
+  sections.querySelectorAll('.nav-section').forEach((section) => {
+    section.setAttribute('aria-expanded', 'false');
+  });
+}
+
+function decorateRegion(section) {
+  // build button
+  const selected = section.querySelector('li strong');
+  const btn = document.createElement('a');
+  btn.classList.add('nav-region-btn');
+  btn.innerHTML = `<span>${selected.textContent}</span>
+    <div class="icon icon-arrow"></div>`;
+  btn.id = 'region-button';
+  btn.setAttribute('aria-expanded', false);
+  btn.setAttribute('aria-haspopup', true);
+  btn.setAttribute('aria-label', 'Change region');
+  btn.setAttribute('role', 'button');
+  btn.setAttribute('tabindex', 0);
+  btn.addEventListener('click', toggleMenu);
+  // decorate options
+  const options = section.querySelector('ul');
+  options.classList.add('nav-region-options');
+  options.setAttribute('aria-labelledby', 'region-button');
+  options.setAttribute('role', 'menu');
+  options.querySelectorAll('a').forEach((a) => {
+    const { pathname: optionPath } = new URL(a.href);
+    const { pathname: windowPath } = new URL(window.location.href);
+    const windowPathArr = windowPath.split('/');
+    windowPathArr[1] = optionPath.replace(/\//g, '');
+    a.href = windowPathArr.join('/');
+  });
+
+  section.prepend(btn);
+  selected.parentNode.remove();
+}
+
+function closeLanguageModal() {
+  const modal = document.querySelector('.nav-language-modal');
+  if (modal) modal.remove();
+}
+
+function redirectToSpanishSite() {
+  const { protocol, pathname } = new URL(window.location.href);
+  window.open(`${protocol}//espanol.kaiserpermanente.org/es${pathname}`);
+}
+
+async function buildLanguageModal() {
+  const resp = await fetch('/northern-california/health-wellness/cancer-care/language-picker.plain.html');
   const html = await resp.text();
-  dialog.innerHTML = html;
+  // build modal
+  const modal = document.createElement('aside');
+  modal.classList.add('nav-language-modal');
+  modal.innerHTML = html;
+  // decorate checkbox
+  const checkbox = modal.querySelector('.checkbox');
+  const label = document.createElement('label');
+  const input = document.createElement('input');
+  input.setAttribute('type', 'checkbox');
+  const span = document.createElement('span');
+  span.classList.add('checkmark');
+  label.textContent = checkbox.textContent;
+  label.prepend(input, span);
+  checkbox.innerHTML = label.outerHTML;
+  checkbox.querySelector('input').addEventListener('change', (e) => {
+    if (e.target.checked) {
+      localStorage.setItem('kp-view-spanish', true);
+    } else {
+      localStorage.removeItem('kp-view-spanish');
+    }
+  });
+  // decorate buttons
+  const btns = modal.querySelectorAll('.button');
+  btns.forEach((btn) => {
+    const button = document.createElement('button');
+    button.textContent = btn.textContent;
+    button.classList.add('btn', 'btn-nav-language-modal');
+    button.id = toClassName(btn.textContent);
+    btn.innerHTML = button.outerHTML;
+  });
+  const cancelBtn = modal.querySelector('#cancelar');
+  if (cancelBtn) cancelBtn.addEventListener('click', closeLanguageModal);
+  const continueBtn = modal.querySelector('#continuar');
+  if (continueBtn) {
+    continueBtn.addEventListener('click', redirectToSpanishSite);
+  }
+  // decorate close btn
   const close = document.createElement('div');
-  close.classList.add('language-picker-close');
-  close.innerHTML = '<div class="language-picker-close-icon"></div>';
-  dialog.prepend(close);
-  document.querySelector('header').append(dialog);
+  close.classList.add('nav-language-modal-close');
+  close.innerHTML = '<div class="nav-language-modal-close-icon"></div>';
+  close.addEventListener('click', closeLanguageModal);
+
+  modal.prepend(close);
+  document.querySelector('header .nav').append(modal);
+}
+
+function decorateLanguage(section) {
+  const btn = section.querySelector('a');
+  btn.removeAttribute('href');
+  btn.addEventListener('click', () => {
+    const showThis = localStorage.getItem('kp-view-spanish');
+    if (!showThis) {
+      buildLanguageModal();
+    } else {
+      redirectToSpanishSite();
+    }
+  });
+}
+
+function closeSearch() {
+  const search = document.querySelector('.nav-search-form');
+  if (search) search.remove();
+}
+
+function clearInput() {
+  const input = document.querySelector('.nav-search-input');
+  if (input) input.value = '';
+}
+
+function openSearch(e) {
+  if (e.code === 'Enter' && e.target.value !== '') {
+    const { origin, pathname } = new URL(window.location.href);
+    const region = pathname.split('/').filter((p) => p !== '').pop();
+    window.open(`${origin}/${region}/pages/search?query=${encodeURIComponent(e.target.value)}&category=All`);
+  }
+}
+
+function displayClear(e) {
+  const clear = document.querySelector('.nav-search-form .icon.icon-close');
+  if (e.target.value.length > 0) {
+    clear.classList.add('show');
+  } else {
+    clear.classList.remove('show');
+  }
+}
+
+function decorateMobileSearch() {
+  const form = document.createElement('div');
+  form.classList.add('nav-search-form', 'nav-search-mobile');
+  // decorate close btn
+  const close = document.createElement('div');
+  close.classList.add('nav-search-close');
+  close.innerHTML = '<div class="nav-search-close-icon"></div>';
+  close.addEventListener('click', closeSearch);
+  // build input
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('nav-search-input-wrapper');
+  const input = document.createElement('input');
+  input.classList.add('nav-search-input');
+  input.addEventListener('keydown', openSearch);
+  input.addEventListener('input', displayClear);
+  wrapper.innerHTML = `<span class="icon icon-search"></span>
+    <span class="icon icon-close"></span>`;
+  const wrapperClose = wrapper.querySelector('.icon.icon-close');
+  wrapperClose.addEventListener('click', clearInput);
+  wrapper.append(input);
+  decorateIcons(wrapper);
+
+  form.append(close, wrapper);
+  document.querySelector('header .nav').append(form);
+}
+
+// function decorateDesktopSearch() { }
+
+function decorateSearch(section) {
+  const btn = section.querySelector('p');
+  btn.addEventListener('click', () => {
+    // const expanded = window.matchMedia('(min-width: 900px)');
+    // if (expanded.matches) {
+    // decorateDesktopSearch();
+    // }
+    // else {
+    decorateMobileSearch();
+    // }
+  });
 }
 
 /**
@@ -34,10 +216,6 @@ async function openLanguageDialog() {
 export default async function decorate(block) {
   const cfg = readBlockConfig(block);
   block.textContent = '';
-
-  // expanded or contracted menu
-  onMediaChange();
-  window.addEventListener('resize', onMediaChange);
 
   // fetch nav content
   const navPath = cfg.nav || '/nav';
@@ -51,10 +229,10 @@ export default async function decorate(block) {
   const navSections = document.createElement('div');
   navSections.classList.add('nav-sections');
   nav.innerHTML = html;
-  const sections = ['brand', 'search', 'title-bar', 'utility-links', 'primary-links'];
+  const sections = ['brand', 'search', 'language', 'region', 'signin', 'links'];
   nav.querySelectorAll(':scope > div').forEach((navSection, i) => {
     if (sections[i]) {
-      // named sections
+      // first section is the brand section
       navSection.classList.add(`nav-${sections[i]}`);
     } else {
       // all other sections
@@ -62,12 +240,30 @@ export default async function decorate(block) {
       navSection.classList.add('nav-section');
       const h2 = navSection.querySelector('h2');
       if (h2) {
-        navSection.classList.add(`nav-${h2.id}`);
-        h2.remove();
+        h2.addEventListener('click', () => {
+          const expanded = navSection.getAttribute('aria-expanded') === 'true';
+          collapseAllNavSections(navSections);
+          navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+        });
       }
     }
   });
+  navSections.append(
+    nav.querySelector('.nav-links').cloneNode(true),
+    nav.querySelector('.nav-signin').cloneNode(true),
+    nav.querySelector('.nav-language').cloneNode(true),
+    nav.querySelector('.nav-region').cloneNode(true),
+  );
   nav.append(navSections);
+
+  const search = nav.querySelector('.nav-search');
+  if (search) decorateSearch(search);
+
+  const regions = nav.querySelectorAll('.nav-region');
+  regions.forEach((region) => decorateRegion(region));
+
+  const languages = nav.querySelectorAll('.nav-language');
+  languages.forEach((language) => decorateLanguage(language));
 
   // hamburger for mobile
   const hamburger = document.createElement('div');
@@ -77,30 +273,12 @@ export default async function decorate(block) {
     const expanded = nav.getAttribute('aria-expanded') === 'true';
     document.body.style.overflowY = expanded ? '' : 'hidden';
     nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
-    const section = nav.querySelector('.nav-sections');
-    if (expanded) {
-      section.innerHTML = '';
-    } else {
-      section.append(
-        nav.querySelector('.nav-utility-links').cloneNode(true),
-        nav.querySelector('.nav-primary-links').cloneNode(true),
-      );
-    }
   });
   nav.prepend(hamburger);
   nav.setAttribute('aria-expanded', 'false');
   decorateIcons(nav);
   block.append(nav);
 
-  // language picker
-  const picker = block.querySelector('a[href*="language-picker"');
-  if (picker) {
-    picker.removeAttribute('href');
-    picker.id = 'language-picker';
-    picker.addEventListener('click', openLanguageDialog);
-  }
-
-  // sign on
-  const signon = block.querySelector('a[href*="signon"');
-  if (signon) signon.classList.add('btn', 'btn-signon');
+  onMediaChange();
+  window.addEventListener('resize', onMediaChange);
 }
