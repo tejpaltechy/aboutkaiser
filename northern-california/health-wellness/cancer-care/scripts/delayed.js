6e74386313a298bce199c95cabf407546fba72ca
David Nuescheler <david@nuescheler.ch>
2022-05-07 15:18:58 -0600

    chore: favicon and launch

--- northern-california/health-wellness/cancer-care/scripts/delayed.js
+++ northern-california/health-wellness/cancer-care/scripts/delayed.js
@@ -5,3 +5,51 @@ import { sampleRUM } from './scripts.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+function loadScript(url, callback, type) {
+  const $head = document.querySelector('head');
+  const $script = document.createElement('script');
+  $script.src = url;
+  if (type) {
+    $script.setAttribute('type', type);
+  }
+  $head.append($script);
+  $script.onload = callback;
+  return $script;
+}
+
+const splits = window.location.pathname.split('/');
+const suffix = splits.join(':');
+const pageName = `kporg:en${suffix}`;
+
+window.digitalData = {
+  page: {
+    pageInfo: {
+      pageName,
+    },
+    category: {
+      primaryCategory: 'kporg',
+      subCategory1: 'kporg:en',
+      subCategory2: splits[1] ? `kporg:en:${splits[1]}` : '',
+      subCategory3: splits[2] ? `kporg:en:${splits[1]}:${splits[2]}` : '',
+      subCategory4: splits[3] ? `kporg:en:${splits[1]}:${splits[2]}:${splits[3]}` : '',
+      subCategory5: splits[4] ? `kporg:en:${splits[1]}:${splits[2]}:${splits[3]}:${splits[4]}` : '',
+    },
+  },
+  user: {
+    profile: {
+    },
+    segment: {
+    },
+  },
+  global: {
+    feature_name: 'Health \x26 wellness',
+    guid: '',
+  },
+};
+
+if (window.location.hash) {
+  window.digitalData.page.pageInfo.pageName = `${pageName}${window.location.hash}`;
+}
+
+loadScript('https://assets.adobedtm.com/launch-ENbd993aa07d4b4467b8a02336a9fe6f2c.min.js');
--- northern-california/health-wellness/cancer-care/scripts/scripts.js
+++ northern-california/health-wellness/cancer-care/scripts/scripts.js
@@ -718,7 +718,7 @@ async function loadLazy(doc) {
   loadFooter(doc.querySelector('footer'));
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
-  addFavIcon(`${window.hlx.codeBasePath}/styles/favicon.svg`);
+  addFavIcon(`${window.hlx.codeBasePath}/icons/favicon.webp`);
 }
 
 /**
